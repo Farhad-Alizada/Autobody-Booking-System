9ef9fa3dc5a2a436494bda98acb70e242779baf4
30187738 <bobby.brar@ucalgary.ca>
2025-04-14 19:17:14 -0600

    Test webpage

---
+++ browser.php
@@ -0,0 +1,14 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WrapLab Auto Styling</title>
+</head>
+<body>
+  <h1>Welcome to WrapLab</h1>
+  <p>Click below to browse our services or book an appointment.</p>
+
+  <a href="auth/login.php">Login</a> |
+  <a href="auth/register.php">Register</a>
+</body>
+</html>
